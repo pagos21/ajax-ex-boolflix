52b4fe9e53a794f9ceaa0557a67524acf88c8909
pagos21 <paga@localhost.yeah>
2020-07-29 15:26:24 +0200

    v 3.1 (Cleaning this mess)

--- JS/script.js
+++ JS/script.js
@@ -11,12 +11,16 @@ function init(){
 // ##### MAIN functions Sec #####
 function writeAPI(userMoviesOrTV){
   var template = $("#template").html();
+  var template2 = $("#template2").html();
+
   var compiled = Handlebars.compile(template);
+  var compiled2 = Handlebars.compile(template2);
+
   var target = $("#cont");
   target.html("");
 
   callFilms(userMoviesOrTV, compiled, target);
-  callTVseries(userMoviesOrTV, compiled, target);
+  callTVseries(userMoviesOrTV, compiled2, target);
 
 }
 
@@ -31,7 +35,7 @@ function callFilms(userMoviesOrTV, compiled, target){
 
     success: function(data, state) {
       var moviesfromAPI = data["results"];
-      insideAPIMovie(moviesfromAPI, compiled, target);
+      insideAPI(moviesfromAPI, compiled, target);
     },
     error:function (error) {
       console.log('error from TV series API');
@@ -39,34 +43,7 @@ function callFilms(userMoviesOrTV, compiled, target){
   }) // ajax
 
 }
-function insideAPIMovie(moviesfromAPI, compiled, target){
-  for (var i = 0; i < moviesfromAPI.length; i++) {
-    // var film = moviesfromAPI[i]
-    var titlefromAPI = moviesfromAPI[i].title;
-    var origTitlefromAPI = moviesfromAPI[i].original_title;
-    var langfromAPI = moviesfromAPI[i].original_language;
-    var votefromAPI = moviesfromAPI[i].vote_average;
-    var coverfromAPI = moviesfromAPI[i].poster_path;
-    var cover2print = "https://image.tmdb.org/t/p/w185/" + coverfromAPI;
-    console.log(cover2print);
-    var voteFrom10to5 = Math.ceil(votefromAPI / 2);
-    var stars = starMultiplier2(voteFrom10to5); //Rendering possibile solo con escape HTML
-    var flag = flagF(langfromAPI);
-
-    var movie2html = compiled({ cover:cover2print,
-                                title:titlefromAPI,
-                                origTitle:origTitlefromAPI,
-                                flag: flag,
-                                voteExtended: votefromAPI,
-                                stars: stars
-                                })
-
-
-    target.append(movie2html);
-  }//for
-}
-
-function callTVseries(userMoviesOrTV, compiled, target){
+function callTVseries(userMoviesOrTV, compiled2, target){
 
   $.ajax({
     url : "https://api.themoviedb.org/3/search/tv?api_key=8b7e062b77e264c1bb194fda0388a653",
@@ -76,7 +53,7 @@ function callTVseries(userMoviesOrTV, compiled, target){
     success: function(data, state) {
       var tvfromAPI = data["results"];
 
-      insideAPItv(tvfromAPI, compiled, target);
+      insideAPI(tvfromAPI, compiled2, target);
 
     },
     error:function (error) {
@@ -84,22 +61,34 @@ function callTVseries(userMoviesOrTV, compiled, target){
     }
   }) // ajax
 }
-function insideAPItv(tvfromAPI, compiled, target){
-  for (var i = 0; i <tvfromAPI.length; i++) {
+function insideAPI(fromAPI, compiled, target){
+  for (var i = 0; i < fromAPI.length; i++) {
     // var film = moviesfromAPI[i]
-    var namefromAPI = tvfromAPI[i].name;
-    var origNamefromAPI = tvfromAPI[i].original_name;
-    var langfromAPI = tvfromAPI[i].original_language;
-    var votefromAPI = tvfromAPI[i].vote_average;
+    var titlefromAPI = fromAPI[i].title;
+    var origTitlefromAPI = fromAPI[i].original_title;
+    var langfromAPI = fromAPI[i].original_language;
+    var votefromAPI = fromAPI[i].vote_average;
+    var coverfromAPI = fromAPI[i].poster_path;
+
+    var namefromAPI = fromAPI[i].name;
+    var origNamefromAPI = fromAPI[i].original_name;
+
+    var cover2print = "https://image.tmdb.org/t/p/w185/" + coverfromAPI;
+    console.log(cover2print);
     var voteFrom10to5 = Math.ceil(votefromAPI / 2);
     var stars = starMultiplier2(voteFrom10to5); //Rendering possibile solo con escape HTML
     var flag = flagF(langfromAPI);
 
-    var movie2html = compiled({ title:namefromAPI,
-                                origTitle:origNamefromAPI,
+    var movie2html = compiled({ cover:cover2print,
+                                title:titlefromAPI,
+                                origTitle:origTitlefromAPI,
+                                name: namefromAPI,
+                                origName: origNamefromAPI,
                                 flag: flag,
                                 voteExtended: votefromAPI,
-                                stars: stars })
+                                stars: stars
+    })
+
 
     target.append(movie2html);
   }//for
